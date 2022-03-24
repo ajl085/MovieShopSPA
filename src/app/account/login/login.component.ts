7aff71fed595762926f67be73ab1e747322e1a0f
ajl085 <ajl085@ucsd.edu>
2022-03-23 23:34:50 -0700

    Added Error messages for invalid login

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,4 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/core/services/authentication.service';
+import { Login } from 'src/app/shared/models/Login';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  invalidLogin: boolean = false;
+  userLogin: Login = {
+    email: '', password: ''
+  }
+
+  constructor(private authService: AuthenticationService, private router: Router) { }
 
   ngOnInit(): void {
+    console.log(this.userLogin);
   }
 
   submit() {
     console.log('submit button clicked');
+    console.log(this.userLogin);
+    // send the data to Authentication service login method
+    this.authService.login(this.userLogin).subscribe(
+      // if success
+      x => {
+        this.router.navigateByUrl('/');
+        this.invalidLogin = false;
+        // 200, 201
+      },
+      (err: HttpErrorResponse) => {
+        // 401, 403, 400
+        this.invalidLogin = true;
+        console.log(err);
+      }
+      // redirect to home page
+      // if error
+      // show the message
+    );
   }
 }
